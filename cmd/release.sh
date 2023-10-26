3ec0cc3c7a13dcfdf1003b5a01a329230c70dd1b
yqk100 <a9k1n9@gmail.com>
2023-10-27 04:53:13 +0800

    Update release.sh

--- cmd/release.sh
+++ cmd/release.sh
@@ -5,9 +5,6 @@ mkdir -p ./bin/release
 
 make() {
     mkdir ./bin/release/daze_$1_$2
-    cp ./README.md ./bin/release/daze_$1_$2/README.md
-    cp ./res/rule.cidr ./bin/release/daze_$1_$2/rule.cidr
-    cp ./res/rule.ls ./bin/release/daze_$1_$2/rule.ls
     GOOS=$1 GOARCH=$2 go build -o ./bin/release/daze_$1_$2 github.com/mohanson/daze/cmd/daze
     python -m zipfile -c ./bin/release/daze_$1_$2.zip ./bin/release/daze_$1_$2
 }
